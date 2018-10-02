e59fe42cd8705c9d1d82b6f93ea2fe21b6b73881
Bela Rosada <belrsd@gmail.com>
2018-10-02 21:38:41 +0800

    Fixing Layout MasterData > CATV Channel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,13 @@
 @extends('layout.layout')
+@section('title')
+    Dashboard
+@endsection
+@section('link1')
+    Home
+@endsection
+@section('link2')
+    Dashboard
+@endsection
 @section('content')
             <a href="#"><img alt="..." src="{{url('assets/images/background.jpg')}}" class="center"></a>
 @endsection
