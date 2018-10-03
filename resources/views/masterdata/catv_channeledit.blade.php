6ae9c8a6c9930035d8d4ac165fcb56c8a8fe776b
Bela Rosada <belrsd@gmail.com>
2018-10-03 14:35:31 +0800

    Fixing Masterdata > Catv Channel

--- resources/views/masterdata/catv_channeledit.blade.php
+++ resources/views/masterdata/catv_channeledit.blade.php
@@ -15,7 +15,14 @@
             <div class="card-body">
                 <form action="{{ url('masterdata/catv_channel/edit') }}" class="form-horizontal">
                     <div class="form-body">
-                        <h3 class="box-title m-t-15">Catv Channel</h3>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <h3 class="box-title m-t-15">Catv Channel</h3>
+                            </div>
+                            <div class="col-md-8">
+                                <button type="button" onclick="history.back()" class="btn-sm btn-success btn-outline pull-right" style="margin-right: 5px"><i class="fa fa-backward"></i>&nbsp;Kembali</button>
+                            </div>
+                        </div>
                         <hr class="m-t-0 m-b-40">
                         <div class="row">
                             <div class="col-md-12">
@@ -30,7 +37,7 @@
                                 <div class="form-group row">
                                     <label class="control-label text-right col-md-3">Frekuensi</label>
                                     <div class="col-md-8">
-                                        <input type="number" name="frekuensi" class="form-control" placeholder="Nilai Frekuensi" value="{{ $rs->frekuensi }}">
+                                        <input type="text" name="frekuensi" class="form-control" placeholder="Nilai Frekuensi" value="{{ $rs->frekuensi }}">
                                     </div>
                                 </div>
                                 <div class="form-group row">
@@ -49,7 +56,7 @@
                                 <div class="row">
                                     <div class="col-md-offset-3 col-md-9">
                                         <button type="submit" class="btn btn-success">Submit</button>
-                                        <button type="button" class="btn btn-inverse">Cancel</button>
+                                        <button type="button" id="btn_cancel" class="btn btn-inverse">Cancel</button>
                                     </div>
                                 </div>
                             </div>
@@ -60,5 +67,13 @@
             </div>
         </div>
     </div>
+
+    @push('scripts')
+    <script>
+        $('#btn_cancel').click(function(){
+            location.reload();
+        })
+    </script>
+    @endpush
 
 @endsection
---
+++ resources/views/masterdata/program.blade.php
@@ -0,0 +1,59 @@
+@extends('layout.layout')
+@section('title')
+    Master Data
+@endsection
+@section('link1')
+    Master Data
+@endsection
+@section('link2')
+    Program TV
+@endsection
+@section('content')
+
+<div class="card col-md-12">
+    <div class="card-body">
+        <div class="row">
+        <!--row-->
+          <div class="col-md-12">
+              <a href="{{ url('masterdata/program/add') }}" id="btn_tambah" class="btn-sm btn-info"><span class="fa fa-plus-square"></span>&nbsp;Tambah Data</a>
+          </div>
+        </div>
+        <div class="table-responsive">
+            <table id="myTable" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th style="text-align: center;">Id</th>
+                        <th style="text-align: center;">Kode Channel</th>
+                        <th style="text-align: center;">Program TV</th>
+                        <th style="text-align: center;">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($rs as $key => $program)
+                    <tr>
+                        <td>{{ $program->id }}</td>
+                        <td>{{ $program->kode_channel }}</td>
+                        <td>{{ $program->program }}</td>
+                        <td width="30%">
+                            <a class="btn btn-warning btn-xs" title="Edit"  href="">
+                                <i class="fa fa-pencil"></i>
+                            </a>
+                            <a class="btn btn-danger btn-xs hapusan" title="Hapus" href="{{ url('masterdata/program/delete', ['id' => $program->id]) }}">
+                                <i class="fa fa-trash"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+@push('script-footer')
+<script type="text/javascript">
+
+</script>
+@endpush
+
+@endsection
