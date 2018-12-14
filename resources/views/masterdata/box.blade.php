44a1fefad5edab4813f3644804f8457630385d39
Bela Rosada <belrsd@gmail.com>
2018-12-14 16:26:41 +0800

    Fixing Masterdata > Box
    layout

--- resources/views/masterdata/box.blade.php
+++ resources/views/masterdata/box.blade.php
@@ -3,57 +3,56 @@
     Master Data
 @endsection
 @section('link1')
-    Master Data
+    Box
 @endsection
 @section('link2')
-    Box
+    List
+@endsection
+@section('link3')
+    <div class="">
+        <a href="{{ url('masterdata/box/add') }}" id="btn_tambah" class="btn-sm btn-info"><span class="fa fa-plus-square"></span>&nbsp;Tambah Data</a>
+    </div>
 @endsection
 @section('content')
 
-<div class="card col-md-12">
-    <div class="card-body">
-        <div class="row">
-        <!--row-->
-          <div class="col-md-12">
-              <a href="{{ url('masterdata/box/add') }}" id="btn_tambah" class="btn-sm btn-info"><span class="fa fa-plus-square"></span>&nbsp;Tambah Data</a>
-          </div>
-        </div>
-        <div class="table-responsive">
-            <table id="myTable" class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th style="text-align: center;">Id</th>
-                        <th style="text-align: center;">Lokasi Area</th>
-                        <th style="text-align: center;">Nama Box</th>
-                        <th style="text-align: center;">Ukuran Box</th>
-                        <th style="text-align: center;">Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($rs as $key => $box)
+    <div class="table-responsive">
+        <table id="myTable" class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th style="text-align: center;">Id</th>
+                    <th style="text-align: center;">Lokasi Area</th>
+                    <th style="text-align: center;">Nama Box</th>
+                    <th style="text-align: center;">Ukuran Box</th>
+                    <th style="text-align: center;">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($rs as $key => $box)
                     <tr>
                         <td>{{ $box->id }}</td>
                         <td>{{ $box->nama_area }}</td>
                         <td>{{ $box->nama_box }}</td>
                         <td>{{ $box->ukuran_box }}</td>
                         <td width="30%">
-                            <a class="btn btn-warning btn-xs" title="Edit"  href="{{ url('masterdata/box/editView', ['id' => $box->id]) }}">
-                                <i class="fa fa-pencil"></i>
+                            <a class="btn btn-warning waves-effect btn-xs" title="Edit"  href="{{ url('masterdata/box/editView', ['id' => $box->id]) }}">
+                                <i class="material-icons">create</i>
                             </a>
-                            <a class="btn btn-danger btn-xs hapusan" title="Hapus" href="{{ url('masterdata/box/delete', ['id' => $box->id]) }}">
-                                <i class="fa fa-trash"></i>
+                            <a class="btn btn-danger waves-effect btn-xs" title="Hapus" href="{{ url('masterdata/box/delete', ['id' => $box->id]) }}">
+                                <i class="material-icons">delete</i>
                             </a>
                         </td>
                     </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                @endforeach
+            </tbody>
+        </table>
     </div>
-</div>
 
-@push('script-footer')
+@push('scripts')
 <script type="text/javascript">
+
+    $(document).ready( function () {
+        $('#myTable').DataTable();
+    });
 
 </script>
 @endpush
--- resources/views/masterdata/boxedit.blade.php
+++ resources/views/masterdata/boxedit.blade.php
@@ -3,80 +3,88 @@
     Master Data
 @endsection
 @section('link1')
-    Master Data
+    Box
 @endsection
 @section('link2')
-    Box Baru
+    Ubah
+@endsection
+@section('link3')
+    <h3 class="">Box</h3>
+@endsection
+@section('link4')
+    <div class="">
+        <button type="button" onclick="history.back()" class="btn-sm bg-cyan pull-right" style="margin-right: 5px"><i class="fa fa-backward"></i>&nbsp;Kembali</button>
+    </div>
 @endsection
 @section('content')
 
-    <div class="col-lg-12">
-        <div class="card card-outline-info">
-            <div class="card-body">
-                <form action="{{ url('masterdata/box/edit') }}" class="form-horizontal">
-                    <div class="form-body">
-                        <h3 class="box-title m-t-15">Box</h3>
-                        <hr class="m-t-0 m-b-40">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <input type="hidden" name="id" value="{{ $rs->id }}">
-                                <input type="hidden" name="nama_box_lama" value="{{ $rs->nama_box }}">
-                                <input type="hidden" name="id_area_lama" value="{{ $rs->id_area }}">
-                                <input type="hidden" name="ukuran_box_lama" value="{{ $rs->ukuran_box }}">
 
-                                <div class="form-group row">
-                                    <label class="control-label text-right col-md-3">Lokasi Area</label>
-                                    <div class="col-md-8">
-                                        <select id="nama_area" name="nama_area" class="form-control custom-select" style="width: 100%">
-                                            @foreach($lokasi_area as $area)
-                                            @if($rs->nama_area == $area->nama_area)
+    <form action="{{ url('masterdata/box/edit') }}" class="form-horizontal">
+        <div class="form-body">
+            <div class="row">
+                <div class="col-md-12">
+                    <input type="hidden" name="id" value="{{ $rs->id }}">
+                    <input type="hidden" name="nama_box_lama" value="{{ $rs->nama_box }}">
+                    <input type="hidden" name="id_area_lama" value="{{ $rs->id_area }}">
+                    <input type="hidden" name="ukuran_box_lama" value="{{ $rs->ukuran_box }}">
+
+                    <div class="form-group row">
+                        <label class="control-label text-right col-md-3">Lokasi Area</label>
+                        <div class="col-md-8">
+                            <div class="form-line">
+                                <select id="nama_area" name="nama_area" class="form-control show-tick" style="width: 100%">
+                                    @foreach($lokasi_area as $area)
+                                        @if($rs->nama_area == $area->nama_area)
                                             <option value="{{$area->id}}" selected="selected">{{$area->nama_area}}</option>
-                                            @else
+                                        @else
                                             <option value="{{$area->id}}">{{$area->nama_area}}</option>
-                                            @endif
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label class="control-label text-right col-md-3">Nama Box</label>
-                                    <div class="col-md-8">
-                                        <input type="text" id="nama_box" name="nama_box" class="form-control" value="{{$rs->nama_box}}">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label class="control-label text-right col-md-3">Ukuran Box</label>
-                                    <div class="col-md-8">
-                                        <input type="text" id="ukuran_box" name="ukuran_box" class="form-control" value="{{$rs->ukuran_box}}">
-                                    </div>
-                                </div>
+                                        @endif
+                                    @endforeach
+                                </select>
                             </div>
                         </div>
                     </div>
-                    <hr>
-                    <div class="form-actions">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="row">
-                                    <div class="col-md-offset-3 col-md-9">
-                                        <button type="submit" class="btn btn-success">Submit</button>
-                                        <button type="button" id="btn_cancel" class="btn btn-inverse">Cancel</button>
-                                    </div>
-                                </div>
+                    <div class="form-group row">
+                        <label class="control-label text-right col-md-3">Nama Box</label>
+                        <div class="col-md-8">
+                            <div class="form-line">
+                                <input type="text" id="nama_box" name="nama_box" class="form-control" value="{{$rs->nama_box}}">
                             </div>
-                            <div class="col-md-6"> </div>
                         </div>
                     </div>
-                </form>
+                    <div class="form-group row">
+                        <label class="control-label text-right col-md-3">Ukuran Box</label>
+                        <div class="col-md-8">
+                            <div class="form-line">
+                                <input type="text" id="ukuran_box" name="ukuran_box" class="form-control" value="{{$rs->ukuran_box}}">
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+        <hr>
+        <div class="form-actions">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-offset-1">
+                            <button type="submit" class="btn btn-success">Submit</button>
+                            <button type="button" id="btn_cancel" class="btn btn-warning">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6"> </div>
+            </div>
+        </div>
+    </form>
 
-    @push('scripts')
+@push('scripts')
     <script>
     $('#btn_cancel').click(function(){
         location.reload();
     })
     </script>
-    @endpush
+@endpush
+
 @endsection
