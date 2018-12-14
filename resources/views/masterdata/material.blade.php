2152195bfe2415723853404ea3a7982be5af08c8
Bela Rosada <belrsd@gmail.com>
2018-12-14 16:27:58 +0800

    Fixing Masterdata > Material
    layout

--- resources/views/masterdata/material.blade.php
+++ resources/views/masterdata/material.blade.php
@@ -3,53 +3,52 @@
     Master Data
 @endsection
 @section('link1')
-    Master Data
+    Material
 @endsection
 @section('link2')
-    Material
+    List
+@endsection
+@section('link3')
+    <div class="">
+        <a href="{{ url('masterdata/material/add') }}" id="btn_tambah" class="btn-sm btn-info"><span class="fa fa-plus-square"></span>&nbsp;Tambah Data</a>
+    </div>
 @endsection
 @section('content')
 
-<div class="card col-md-12">
-    <div class="card-body">
-        <div class="row">
-        <!--row-->
-          <div class="col-md-12">
-              <a href="{{ url('masterdata/material/add') }}" id="btn_tambah" class="btn-sm btn-info"><span class="fa fa-plus-square"></span>&nbsp;Tambah Data</a>
-          </div>
-        </div>
-        <div class="table-responsive">
-            <table id="myTable" class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th style="text-align: center;">Id</th>
-                        <th style="text-align: center;">Nama Material</th>
-                        <th style="text-align: center;">Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($rs as $key => $material)
+    <div class="table-responsive">
+        <table id="myTable" class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th style="text-align: center;">Id</th>
+                    <th style="text-align: center;">Nama Material</th>
+                    <th style="text-align: center;">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($rs as $key => $material)
                     <tr>
                         <td>{{ $material->id }}</td>
                         <td>{{ $material->nama_material }}</td>
                         <td width="30%">
-                            <a class="btn btn-warning btn-xs" title="Edit"  href="{{ url('masterdata/material/editView', ['id' => $material->id]) }}">
-                                <i class="fa fa-pencil"></i>
+                            <a class="btn btn-warning waves-effect btn-xs" title="Edit"  href="{{ url('masterdata/material/editView', ['id' => $material->id]) }}">
+                                <i class="material-icons">create</i>
                             </a>
-                            <a class="btn btn-danger btn-xs hapusan" title="Hapus" href="{{ url('masterdata/material/delete', ['id' => $material->id]) }}">
-                                <i class="fa fa-trash"></i>
+                            <a class="btn btn-danger waves-effect btn-xs" title="Hapus" href="{{ url('masterdata/material/delete', ['id' => $material->id]) }}">
+                                <i class="material-icons">delete</i>
                             </a>
                         </td>
                     </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                @endforeach
+            </tbody>
+        </table>
     </div>
-</div>
 
-@push('script-footer')
+@push('scripts')
 <script type="text/javascript">
+
+$(document).ready( function () {
+    $('#myTable').DataTable();
+});
 
 </script>
 @endpush
--- resources/views/masterdata/materialedit.blade.php
+++ resources/views/masterdata/materialedit.blade.php
@@ -3,58 +3,61 @@
     Master Data
 @endsection
 @section('link1')
-    Master Data
+    Material
 @endsection
 @section('link2')
-    Material Baru
+    Ubah
+@endsection
+@section('link3')
+    <h3 class="">Material</h3>
+@endsection
+@section('link4')
+    <div class="">
+        <button type="button" onclick="history.back()" class="btn-sm bg-cyan pull-right" style="margin-right: 5px"><i class="fa fa-backward"></i>&nbsp;Kembali</button>
+    </div>
 @endsection
 @section('content')
 
-    <div class="col-lg-12">
-        <div class="card card-outline-info">
-            <div class="card-body">
-                <form action="{{ url('masterdata/material/edit') }}" class="form-horizontal">
-                    <div class="form-body">
-                        <h3 class="box-title m-t-15">Material</h3>
-                        <hr class="m-t-0 m-b-40">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <input type="hidden" name="id" value="{{ $rs->id }}">
-                                <input type="hidden" name="material_lama" value="{{ $rs->nama_material }}">
+    <form action="{{ url('masterdata/material/edit') }}" class="form-horizontal">
+        <div class="form-body">
+            <div class="row">
+                <div class="col-md-12">
+                    <input type="hidden" name="id" value="{{ $rs->id }}">
+                    <input type="hidden" name="material_lama" value="{{ $rs->nama_material }}">
 
-                                <div class="form-group row">
-                                    <label class="control-label text-right col-md-3">Nama Material</label>
-                                    <div class="col-md-8">
-                                        <input type="text" id="nama_material" name="nama_material" class="form-control" value="{{$rs->nama_material}}">
-                                    </div>
-                                </div>
+                    <div class="form-group row">
+                        <label class="control-label text-right col-md-3">Nama Material</label>
+                        <div class="col-md-8">
+                            <div class="form-line">
+                                <input type="text" id="nama_material" name="nama_material" class="form-control" value="{{$rs->nama_material}}">
                             </div>
                         </div>
                     </div>
-                    <hr>
-                    <div class="form-actions">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="row">
-                                    <div class="col-md-offset-3 col-md-9">
-                                        <button type="submit" class="btn btn-success">Submit</button>
-                                        <button type="button" id="btn_cancel" class="btn btn-inverse">Cancel</button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6"> </div>
-                        </div>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
-    </div>
+        <hr>
+        <div class="form-actions">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-offset-1">
+                            <button type="submit" class="btn btn-success">Submit</button>
+                            <button type="button" id="btn_cancel" class="btn btn-warning">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6"> </div>
+            </div>
+        </div>
+    </form>
 
-    @push('scripts')
+@push('scripts')
     <script>
-        $('#btn_cancel').click(function(){
-            $('#nama_material').val('')
-        })
+    $('#btn_cancel').click(function(){
+        $('#nama_material').val('')
+    })
     </script>
-    @endpush
+@endpush
+
 @endsection
